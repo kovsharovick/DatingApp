cc4cde4afa9020870dd22d4365ed58420d634a91
SkyDreameme <gerashchenko-nastyusha@mail.ru>
2026-05-06 10:57:09 +0300

    basic feed (messages, profile plugs)

--- android/app/src/main/java/com/example/androiddatingapp/MainActivity.kt
+++ android/app/src/main/java/com/example/androiddatingapp/MainActivity.kt
@@ -1,47 +1,43 @@
 package com.example.androiddatingapp
 
 import android.os.Bundle
+import android.util.DisplayMetrics
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.androiddatingapp.ui.AppRoot
+import com.example.androiddatingapp.ui.model.ScreenInfo
 import com.example.androiddatingapp.ui.theme.AndroidDatingAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val displayMetrics = DisplayMetrics().also { dm ->
+            @Suppress("DEPRECATION")
+            windowManager.defaultDisplay.getRealMetrics(dm)
+        }
+        val screen = ScreenInfo(
+            widthPx = displayMetrics.widthPixels,
+            heightPx = displayMetrics.heightPixels,
+            density = displayMetrics.density
+        )
+
         setContent {
             AndroidDatingAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Surface(Modifier.fillMaxSize().padding(innerPadding)) {
+                        AppRoot(screen = screen)
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidDatingAppTheme {
-        Greeting("Android")
-    }
-}
